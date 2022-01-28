6fd41d7d6727fa16a99d1dcfdfe2470840759467
Oleksandr Datsiuk <oleksandr_datsiuk@epam.com>
2022-01-28 18:08:08 +0200

    comments added

--- home_task1.py
+++ home_task1.py
@@ -8,41 +8,43 @@ for i in range(100):
 # use method appent add to the list 1000 values. raindit-existing method in random library
     a_list.append(random.randint(0, 1000))
 
-#
+# start sorting. Set for variable k a start number to the value of a_list
 for k in range(len(a_list)):
+    #set for variable j value k+1 for a_list
     for j in range(k + 1, len(a_list)):
-# comparison k and j what the biggest
+# comparison k and j what the biggest?
         if a_list[k] > a_list[j]:
 # change place if one bigger than other one
            a_list[k], a_list[j] = a_list[j], a_list[k]
 
 print (a_list)
 
-# define counters
+# define variables for sum even and odd values for using in average counting
 even_sum, odd_sum = 0, 0
 for num in a_list:
-    # even numbers
+    # sum of even
     if num % 2 == 0:
         even_sum += num
-    # odd numbers
+    # sum of odd
     else:
         odd_sum += num
 
 print("Sum of Even numbers in the list: ", even_sum)
 print("Sum of Odd numbers in the list: ", odd_sum)
-
+# define variables for count even and odd values for using in average counting
 even_count, odd_count = 0, 0
 for cnt in a_list:
-    # even numbers
+    # count of even
     if cnt % 2 == 0:
         even_count += 1
-    # odd numbers
+    # count of odd
     else:
         odd_count += 1
 
 print("Count of Even numbers in the list: ", even_count)
 print("Count of Odd numbers in the list: ", odd_count)
 
+#average count
 averg_even = even_sum/even_count
 averg_odd = odd_sum/odd_count
 
