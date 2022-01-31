f76bcfd30730f8f15c47bb0f9b90884e72787535
Oleksandr Datsiuk <oleksandr_datsiuk@epam.com>
2022-01-31 22:55:58 +0200

    home_task2

--- home_task2.py
+++ home_task2.py
@@ -1,20 +1,21 @@
 from random import randint, choice
 from string import ascii_lowercase
+#create dictionary with random list using random library and method randint
+#generate id's as letters using string library
+rnd_list = [{choice(ascii_lowercase): randint(0, 100) for i in range(len(ascii_lowercase))} for j in range(randint(2, 3))]
+print(rnd_list)
 
-final_dict, tmp_dict = {}, {}
+result_dict, tmp_dict = {}, {}
+for dictionary in rnd_list:
+ # assign each id particaular values from dict
+  for k, m in dictionary.items():
+    tmp_dict.setdefault(k, []).append(m)
 
-rand_list = [{choice(ascii_lowercase): randint(0, 100) for i in range(len(ascii_lowercase))} for j in range(randint(2, 3))]
+# Define the biggest values from all dict if ids are duplicated in several dict
+for k, m in tmp_dict.items():
+  if len(m) > 1:
+    result_dict[k+"_"+str(m.index(max(m))+1)] = max(m)
+# if id is distinct (not duplicated), print it without '_index dict'
+  else: result_dict[k] = m[0]
 
-print (rand_list)
-for dictionary in rand_list:
-  for k, v in dictionary.items():
-    tmp_dict.setdefault(k, []).append(v)
-
-#Now choose only the biggest one
-for k, v in tmp_dict.items():
-  if len(v) > 1:
-    final_dict[k+"_"+str(v.index(max(v))+1)] = max(v)
-  else: final_dict[k] = v[0]
-
-
-print(final_dict)
+print(result_dict)
