e9cd16ef36ce4bf95ddc67dd7025a5325b8b7017
Oleksandr Datsiuk <oleksandr_datsiuk@epam.com>
2022-02-02 20:14:33 +0200

    comments added

--- home_task2.py
+++ home_task2.py
@@ -8,16 +8,19 @@ print(rnd_list)
 # define two additional dict
 result_dict, tmp_dict = {}, {}
 for dictionary in rnd_list:
- # assign each id particular values from dict
+# assign each id (letter) particular values from dict
   for k, m in dictionary.items():
-#setdefault() method returns the value of a key (if the key is in dictionary). If not, it inserts key with a value to the dictionary
+#setdefault() method returns the value of a key from dictionary
+#using append() method to  adds a single item to existing id (e.g. k:'g' m:[12])
     tmp_dict.setdefault(k, []).append(m)
 
-# Define the biggest values from all dict if ids are duplicated in several dict
+# work with temp dict to find out the biggest values from all dict if ids are duplicated in several dict
 for k, m in tmp_dict.items():
+# find out if values 'm' for id 'k' more than one
   if len(m) > 1:
+# add '_' for duplicated id (according the task choose max value for duplicated ID) and also add +1 for index of list because countig begining from the zero
     result_dict[k+"_"+str(m.index(max(m))+1)] = max(m)
-# if id is distinct (not duplicated), print it without '_index dict'
+# if id is distinct (not duplicated), print result without '_index dict'
   else: result_dict[k] = m[0]
-
-print(result_dict)/
+#print result dict
+print(result_dict)
