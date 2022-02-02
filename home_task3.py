bccf522432c49565d3b8f2ffd25a78491e926155
Oleksandr Datsiuk <oleksandr_datsiuk@epam.com>
2022-02-03 01:01:10 +0200

    home task3

--- home_task3.py
+++ home_task3.py
@@ -1,4 +1,5 @@
-
+import re
+import string
 old_text ="""homEwork:
 
   tHis iz your homeWork, copy these Text to variable.
@@ -9,15 +10,30 @@ old_text ="""homEwork:
 
 
 
-  it iZ misspeLLing here. fix“iZ“ with correct “is, but ONLY when it Iz a mistAKE.
+  it iZ misspeLLing here. fix“iZ“ with correct “is“, but ONLY when it Iz a mistAKE.
 
 
 
   last iz TO calculate nuMber OF Whitespace characteRS in this Tex. caREFULL, not only Spaces, but ALL whitespaces. I got 87."""
 
-#i=old_string.replace('“','"')
-i=old_text.lower()
-i.capitalize()
-#j=whitespaces(old_text)
-print(i)
+count=0
+for i in old_text:
+   if  i==' ' or i=='\n' or i=='\t':
+      count=count+1
+print('Count of Whitespace in default text is:',count)
+#for cng_quotes in old_text:
+#    old_text.replace('“', '"')
+cng_quotes=old_text.replace('“','"')
+low_case=cng_quotes.lower()
+cap_first_char= low_case.capitalize()
+#
+last = re.split(r'[\s]', cap_first_char)  # list of words
+# result = re.findall(r'\w*',cap_first_char)
+print(' '.join('\b*\.\b',last))
+
+
+
+# lst_words = re.search(r'\w+\.', last)
+#print(last)
+# print(lst_words)
 
